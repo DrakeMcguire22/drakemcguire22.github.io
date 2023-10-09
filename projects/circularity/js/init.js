a2ee767562f5b8df9b51ea8d812a15d5067e17bf
Drake McGuire <drakemcguire33@gmail.com>
2023-10-09 12:38:13 +0000

    f

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        // Code to draw a circle
+        function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas, 10, 10);
+        view.addChild(circle);
+        circles.push(circle);
+        };
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        
+        for(var i = 0; i < 101; i++){
+            drawCircle();
+        }
+        
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +50,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            for(var i = 0; i < circles.length; i++){
+                var eachCircle = circles[i];
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            
+
 
             // TODO 9 : Iterate over the array
            
@@ -62,7 +78,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.y > canvas.height){
+                circle.y = 0;
+            }
+            if (circles.x < 0) {
+                circle.x = 0;
+            }
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
@@ -90,3 +114,6 @@ if((typeof process !== 'undefined') &&
     // here, export any references you need for tests //
     module.exports = init;
 }
+
+
+console.log("Drake is a lame");
